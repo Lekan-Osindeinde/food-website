6214178dd741aaa66595fe73a975f9fffc7e911a
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-04-12 03:00:40 +0100

    Footer styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" layout="fill" alt="" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            OH YES, WE DID THE YUMMY PIZZA, WELL BBAKED SLICE OF PIZZA
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <p className={styles.text}>
+            Hundisburger Strasse #304.
+            <br /> Magdeburg, 39124
+            <br /> (405) 891-2020
+          </p>
+          <p className={styles.text}>
+            Alter Markt Strasse #304.
+            <br /> Magdeburg, 39174
+            <br /> (405) 891-2020
+          </p>
+          <p className={styles.text}>
+            Leipziger Strasse #304.
+            <br /> Magdeburg, 39224
+            <br /> (405) 891-2020
+          </p>
+          <p className={styles.text}>
+            Operhaus Strasse #304.
+            <br /> Magdeburg, 39424
+            <br /> (405) 891-2020
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 12:00 - 24:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
