906623676aebd2488fb424ac467bf7a4f131ab3e
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-04-16 00:46:12 +0100

    delete

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs-pages",
+ 
 };
 
 module.exports = nextConfig;
