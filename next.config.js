b4299a1f34163acfd9201c2e686e020455ff42ef
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-04-16 03:01:42 +0100

    removal of loader

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  basePath: "food-website",
-  assetPrefix: "food-website",
 };
 
 module.exports = nextConfig;
